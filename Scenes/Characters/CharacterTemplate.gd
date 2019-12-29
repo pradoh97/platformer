258b5b3efcc65701eb48d89fa00a9a4ce718ba04
Hernan <pradoh97@gmail.com>
2019-12-29 19:09:41 -0300

    jump

--- Scenes/Characters/CharacterTemplate.gd
+++ Scenes/Characters/CharacterTemplate.gd
@@ -3,7 +3,14 @@ extends KinematicBody2D
 const ACCELERATION = 100
 const GRAVITY = 100
 const MAX_SPEED = 600
-const JUMP_SPEED = 300
+const JUMP_SPEED = -1500
 const FRICTION = 0.2
+const UP = Vector2(0,-1)
 
 var motion = Vector2()
+
+func apply_gravity():
+	if not is_on_floor():
+		motion.y += GRAVITY
+	else:
+		motion.y = 0
--- Scenes/Characters/Player.gd
+++ Scenes/Characters/Player.gd
@@ -1,20 +1,19 @@
 extends "res://Scenes/Characters/CharacterTemplate.gd"
 
 func _physics_process(delta):
-	walk()
+	apply_gravity()
 	jump()
-	motion = move_and_slide(motion)
+	walk()
+	motion = move_and_slide(motion, UP)
 
 func walk():
 	if Input.is_action_pressed("move_right") and not Input.is_action_pressed("move_left"):
 		motion.x = clamp(motion.x + ACCELERATION, 0, MAX_SPEED)
 	elif Input.is_action_pressed("move_left") and not Input.is_action_pressed("move_right"):
-		motion.x = clamp(motion.x-ACCELERATION, -MAX_SPEED, 0)
+		motion.x = clamp(motion.x - ACCELERATION, -MAX_SPEED, 0)
 	else:
 		motion.x = lerp(motion.x, 0, FRICTION)
 
 func jump():
-	if Input.is_action_pressed("jump"):
-		motion.y -= JUMP_SPEED
-	else:
-		motion.y = 0
+	if Input.is_action_pressed("jump") and is_on_floor():
+		motion.y = JUMP_SPEED
