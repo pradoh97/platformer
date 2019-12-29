3e52a3b84417fdf4abd6e9e4c5c788a492848acc
Hernan <pradoh97@gmail.com>
2019-12-29 19:00:03 -0300

    movement and character template

--- Scenes/Characters/Player.gd
+++ Scenes/Characters/Player.gd
@@ -6,9 +6,9 @@ func _physics_process(delta):
 	motion = move_and_slide(motion)
 
 func walk():
-	if Input.is_action_pressed("move_rigth"):
+	if Input.is_action_pressed("move_right") and not Input.is_action_pressed("move_left"):
 		motion.x = clamp(motion.x + ACCELERATION, 0, MAX_SPEED)
-	elif Input.is_action_pressed("move_left"):
+	elif Input.is_action_pressed("move_left") and not Input.is_action_pressed("move_right"):
 		motion.x = clamp(motion.x-ACCELERATION, -MAX_SPEED, 0)
 	else:
 		motion.x = lerp(motion.x, 0, FRICTION)
